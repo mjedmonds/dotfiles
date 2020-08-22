2d92fdbe61ed11e21a8e0ca54232ee6b5857b5f0
Mark Edmonds <mark@mjedmonds.com>
2020-08-22 18:55:34 -0500

    updated adding google static routes to delete any old static routes

--- scripts/add-google-static-routes.sh
+++ scripts/add-google-static-routes.sh
@@ -1,17 +1,44 @@
 #!/bin/bash
 
-if [[ "$OSTYPE" = "darwin"* ]]; then
-  # assumes gateway is XXX. followed by the rest of the gateway
+function add_static_route_macos {
+  ip=$1
+  ip_base=$2
+  ## remove old static routes if they exist
+  gateway_old=$(netstat -rn | grep ${ip_base} | awk '{print $2}')
+  if [ -n "$gateway_old" ]; then
+    sudo route -nv delete $ip $gateway_old
+    echo "Deleted old static route from $ip to $gateway_old"
+  fi
+
+  ## add new static route
   gateway=$(netstat -rn | awk '/default/ {if ( index($2, ".") > 0 ){print $2} }')
+  sudo route -nv add $ip $gateway
+}
+
+function add_static_route_ubuntu {
+  ip=$1
+  gateway_old=$(ip route list | grep ${ip} | awk '{print $3}')
+  if [ -n "$gateway_old" ]; then
+    sudo ip route delete $ip via $gateway_old
+    echo "Deleted old static route from $ip to $gateway_old"
+  fi
+
+  ## add new static route
+  gateway=$(ip route list | awk ' /^default/ {print $3; exit(0)}')
+  sudo ip route add $ip via $gateway
+}
+
+if [[ "$OSTYPE" = "darwin"* ]]; then
+  ## add new static route
   # general google
-  sudo route -nv add 172.217.0.0 ${gateway}
+  add_static_route_macos 172.217.0.0/16 172.250
   # google scholar
-  sudo route -nv add 142.250.0.0 ${gateway}
+  add_static_route_macos 142.250.0.0/16 142.250
 fi
 if [[ "$OSTYPE" = "linux"* ]]; then
-  gateway=$(ip route list | awk ' /^default/ {print $3; exit(0)}')
   # general google
-  sudo ip route add 172.217.0.0/16 via $gateway
+  add_static_route_ubuntu 172.217.0.0/16
   # google scholar
-  sudo ip route add 142.250.0.0/16 via $gateway
+  add_static_route_ubuntu 142.250.0.0/16
+  add_static_route_ubuntu 216.239.32.0/19
 fi
